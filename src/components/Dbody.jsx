e24194754c13daf5a03873b425f1548b99e0cbc7
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 18:28:20 -0500

    Se Annade el outlet

--- src/components/Dbody.jsx
+++ src/components/Dbody.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
 import './Dbody.css';
-import Pizarra from "./Pizarra";
-import Estadistica from "./Estadistica";
-import {Route, Routes} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 
-function Dbody({authRoutes}) {
-    return (
-        <div className={'box'}>
-            <Routes>
-                {authRoutes.map(({path, page}) => (
-                    <Route key={path} path={path} element={page}/>
-                ))}
-            </Routes>
-        </div>
-    );
+function Dbody() {
+    return (<div className={'box'}>
+        <Outlet/>
+    </div>);
 }
 
 export default Dbody;
