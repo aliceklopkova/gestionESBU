5772b539856220e17a8608fb9f8a80fbad9bbd0b
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 18:30:28 -0500

    Actualizar dashboard

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Navbarapp from "./Navbar";
+import Buttonsgroup from "./Buttonsgroup";
+import Dbody from "./Dbody";
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbarapp/>
+            <Buttonsgroup/>
+            <Dbody/>
+        </div>
+    )
+}
+export default Dashboard
