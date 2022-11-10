065bdaa504092e46fc7f8685b61b5981793e19fb
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 18:30:49 -0500

    Actualizar rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,12 +8,40 @@ import EstudianteModel from "../models/estudianteModel";
 import CustomForm from "../components/Customformulario";
 import formFieldDescription from "../data/formFieldDescription";
 import estudianteApi from "../api/estudiante";
-
+import Dashboard from "../components/Dashboard";
+import AlertaEliminar from "../components/AlertaEliminar";
+import LoginComponente from "../components/Logincomponent";
 
 
 export const authRoutes = [
-    {path: "/", page: <Estadistica/>},
-    {path: "/estudiante", page: <BasicTable fields={estudianteFields} model={EstudianteModel} api={estudianteApi}/>},
-    {path: "/estudiante/Formulario", page: <CustomForm  fields={estudianteFields}  />},
+    {
+        path: "/", element: <Dashboard/>, children: [
+            {
+                path: "/home",
+                element: <Estadistica/>
+            },
+            {
+                path: "/estudiante",
+                element: <BasicTable fields={estudianteFields} model={EstudianteModel} api={estudianteApi}/>,
+                children: [
+                    {
+                        path: "/estudiante/:id/delete",
+                        element: <AlertaEliminar api={estudianteApi}/>
+                    },]
+            },
+            {
+                path: "/estudiante/add",
+                element: <CustomForm fields={estudianteFields} model={EstudianteModel} api={estudianteApi}/>
+            },
+            {
+                path: "/estudiante/:id",
+                element: <CustomForm fields={estudianteFields} model={EstudianteModel} api={estudianteApi}
+                                     modificar={true}/>
+            },
+
+        ]
+    },
 ];
-export const publicRoutes = [];
+export const publicRoutes = [
+    {path: "/login", element:<LoginComponente/>}
+];
