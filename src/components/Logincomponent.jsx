a85cae15b8e00838c2faf4e99b7aac0082c92a53
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 18:33:15 -0500

    Agregar login component

---
+++ src/components/Logincomponent.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {Col, Button, Row, Container, Card, Form} from "react-bootstrap";
+import './Logincomponent.css';
+
+function LoginComponente() {
+    return (
+        <div>
+            <Container>
+                <Row className="vh-100 d-flex justify-content-center ">
+                    <Col style={{marginTop: "94px"}} md={8} lg={6} xs={12}>
+                        <div>
+
+                        </div>
+                        <Card className="shadow">
+                            <Card.Body>
+                                <div className="mb-3 mt-md-4">
+                                    <h2 style={{textAlign: "center"}}
+                                        className="fw-bold mb-2 text-uppercase ">Bienvenido <img
+                                        alt=""
+                                        src={"./logoblanco.png"}
+                                        width="30"
+                                        height="30"
+                                        className="d-inline-block align-top"
+                                    /></h2>
+                                    <p style={{textAlign: "center"}} className=" mb-5">Por favor escriba su usuario y
+                                        contraseña para autenticarse!</p>
+                                    <div className="mb-3">
+                                        <Form>
+                                            <Form.Group className="mb-3"
+                                                        controlId="formBasicEmail">
+                                                {/*<Form.Label >*/}
+                                                {/*    Usuario*/}
+                                                {/*</Form.Label>*/}
+                                                <Form.Control size="lg" placeholder="Usuario*" type="text" required/>
+                                            </Form.Group>
+
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicPassword"
+                                            >
+                                                {/*<Form.Label>Contraseña</Form.Label>*/}
+                                                <Form.Control size="lg" type="password" placeholder="Contraseña*"
+                                                              required/>
+                                            </Form.Group>
+                                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                                                <Form.Check type="checkbox" label="Recordar"/>
+                                            </Form.Group>
+                                            <div className="d-grid">
+                                                <Button variant="success" type="submit">
+                                                    Iniciar sesión
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                     <footer style={{textAlign:"center"}}> &copy; Copyright 2022, Alicia </footer>
+                </Row>
+
+            </Container>
+
+
+        </div>
+    );
+}
+
+export default LoginComponente;
