ab3495493b8e9b2351a26b68c61e6a625f2afa0e
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 17:41:01 -0500

    se agregaron las rutas en el componente App.js y se agrego la clase app-content al css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Button from 'react-bootstrap/Button';
 import Navbarapp from "./components/Navbar";
 import Buttonsgroup from "./components/Buttonsgroup";
 import Dbody from "./components/Dbody";
-import {authRoutes} from "./routes/index"
-
+import {authRoutes, publicRoutes} from "./routes/index"
+import {useRoutes} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
 
 function App() {
+    const routes = useRoutes([
+        ...authRoutes,
+        ...publicRoutes
+    ])
     return (
-        <div >
-            <Navbarapp/>
-            <Buttonsgroup/>
-            <Dbody authRoutes={authRoutes}/>
+        <div>
+            {routes}
         </div>
     );
 }
