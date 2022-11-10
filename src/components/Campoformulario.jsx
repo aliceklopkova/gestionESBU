e4eb32cb8cc50fdfff08d74474856cb29ef9750d
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 18:14:32 -0500

    metodo que coge los cambios que se realizan en los campos del formulario

--- src/components/Campoformulario.jsx
+++ src/components/Campoformulario.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -7,12 +7,19 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import './Campoformulario.css';
 
-function CampoFormulario({data}) {
+function CampoFormulario({data, setData, campo}) {
+    const handleChange = (event) => {
+        const {value, name} = event.target
+        setData(oldData => {
+            return {...oldData, [name]: value}
+        })
+    }
     return (
         <div className={'posicion'}>
             <Form.Group className="mb-3" lg="6" md="6" sm="6" controlId={data.id}>
                 <Form.Label>{data.name}</Form.Label>
-                <Form.Control type={data.type} placeholder={data.secondEntry}/>
+                <Form.Control name={campo} type={data.type} placeholder={data.secondEntry} onChange={handleChange}
+                              defaultValue={data.values}/>
                 <Form.Text className="text-muted">
                     {data.text}
                 </Form.Text>
