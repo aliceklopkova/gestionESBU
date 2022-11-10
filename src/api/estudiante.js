a8c60546aa9e96179dcd2daf89dd2f710faa8e3c
Alicia Klopkova <aaportela73@gmail.com>
2022-11-10 18:31:14 -0500

    Actualizar estudiante api

--- src/api/estudiante.js
+++ src/api/estudiante.js
@@ -3,8 +3,9 @@ import getApi from "./base/get-api";
 const {REACT_APP_BACKEND_API} = process.env;
 const endpoint = `${REACT_APP_BACKEND_API}/estudiante/`;
 
-const estudianteApi = {
-    ...getApi(endpoint)
-}
+const estudianteApi = (id) => (
+    {...getApi(endpoint, id)}
+)
+
 
 export default estudianteApi
