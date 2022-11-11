dcf9736dfc6134ef4718242e07645aeab08fa2e7
Alicia Klopkova <aaportela73@gmail.com>
2022-11-11 03:58:59 -0500

    Agregue rendering conditional dependiendo de la authentication

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,8 +2,14 @@ import React from "react"
 import Navbarapp from "./Navbar";
 import Buttonsgroup from "./Buttonsgroup";
 import Dbody from "./Dbody";
+import {useSelector} from "react-redux";
+import {Navigate} from "react-router-dom";
 
 const Dashboard = () => {
+    const isAuthenticated = useSelector(state => state.user.isAuthenticated)
+    if (!isAuthenticated) {
+        return <Navigate to="/login"/>
+    }
     return (
         <div>
             <Navbarapp/>
