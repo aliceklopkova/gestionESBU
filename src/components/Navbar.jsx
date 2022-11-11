372956cb20a55696081d1ab448cab7dc7a558eba
Alicia Klopkova <aaportela73@gmail.com>
2022-11-11 04:05:45 -0500

    Agregar Logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,26 +2,33 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import {Link} from "react-router-dom";
 
-function Navbarapp () {
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="">
-            <img
-              alt=""
-              src={"./logonegro.png"}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{' '}
-            GEDUCA
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
-    </>
-  );
+function Navbarapp() {
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="">
+                        <img
+                            alt=""
+                            src={"./logonegro.png"}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        GEDUCA
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <Link to="/logout">Logout</Link>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
 
 export default Navbarapp;
