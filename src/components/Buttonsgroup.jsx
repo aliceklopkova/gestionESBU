691d4d2bab07d18cd10fdb4f5336a1a2bc458be6
Alicia Klopkova <aaportela73@gmail.com>
2022-11-13 16:41:04 -0500

    Se uso un icono de material

--- src/components/Buttonsgroup.jsx
+++ src/components/Buttonsgroup.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Buttonsgroup.css';
 import {Link} from "react-router-dom";
+import HomeIcon from '@mui/icons-material/Home';
 
 
 function Buttonsgroup() {
   return (
       <div className="sidebar">
-          <Link to={"/home"}>Home</Link>
+          <Link  to={"/home"}><HomeIcon /><span>Home</span></Link>
           <Link to={"/estudiante"}>Estudiantes</Link>
           <Link to={"/profesor"}>Profesores</Link>
 
