e80108669eaf8654d3c9d35963c0aa5ebe66aa11
Abel Cuza <abel.cuza07@gmail.com>
2022-11-16 01:23:22 -0500

    Update layout code and improve spaces

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,36 +4,28 @@ import Navbar from 'react-bootstrap/Navbar';
 import {Link} from "react-router-dom";
 import './Navbar.css';
 
-function Navbarapp() {
-    return (
-        <>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="">
-                        <img
-                            alt=""
-                            src={"./logonegro.png"}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        GEDUCA
-                    </Navbar.Brand>
-                    <Navbar.Toggle/>
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                            <Link style={{textDecoration: "none"}} to="/logout"><span style={{paddingRight:"5px"}} className="material-symbols-outlined">
-account_circle
-</span><span
-                                className="material-symbols-outlined">
-logout
-</span></Link>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    );
-}
+const Navbarapp = () => (
+    <Navbar bg="dark" variant="dark">
+        <Container>
+            <Navbar.Brand href="">
+                <img alt="" src={"./logonegro.png"} width="30" height="30" className="d-inline-block align-top"/>
+                GEDUCA
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+            <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                    <Link style={{textDecoration: "none"}} to="/logout">
+                        <span style={{paddingRight: "5px"}} className="material-symbols-outlined">
+                            account_circle
+                        </span>
+                        <span className="material-symbols-outlined">
+                            logout
+                        </span>
+                    </Link>
+                </Navbar.Text>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
 
 export default Navbarapp;
