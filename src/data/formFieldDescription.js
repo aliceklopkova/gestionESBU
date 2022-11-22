1def7192ec2c11d6da78b1560b28cceaa5e86017
Abel Cuza <abel.cuza07@gmail.com>
2022-11-21 22:35:05 -0500

    Update form fields and table fields

--- src/data/formFieldDescription.js
+++ src/data/formFieldDescription.js
@@ -1,117 +1,278 @@
+const today = new Date()
 const formFieldDescription = {
     nombre: {
-        id: "FormControlNombre",
-        name: "Nombre",
+        id: "nombre",
+        label: "Nombre",
         type: "text",
-        secondEntry: "Escriba el nombre ",
-        text: "",
+        helperText: "Escriba el nombre ",
+        required: true,
     },
     primer_apellido: {
-        id: "FormControlApellido1",
-        name: "Primer Apellido",
+        id: "primer_apellido",
+        label: "Primer Apellido",
         type: "text",
-        secondEntry: "Escriba el primer apellido ",
-        text: "",
+        helperText: "Escriba el primer apellido ",
+        required: true,
     },
     segundo_apellido: {
-        id: "FormControlApellido2",
-        name: "Segundo Apellido",
+        id: "segundo_apellido",
+        label: "Segundo Apellido",
         type: "text",
-        secondEntry: "Escriba el segundo apellido ",
-        text: "",
+        helperText: "Escriba el segundo apellido ",
+        required: true,
     },
     fecha_nacimiento: {
-        id: "FormControlNacimiento",
-        name: "Fecha de nacimiento",
+        id: "fecha_nacimiento",
+        label: "Fecha de nacimiento",
         type: "date",
-        secondEntry: "Escriba la fecha de nacimiento ",
-        text: "",
+        helperText: "Escriba la fecha de nacimiento ",
+        required: true,
+        defaultValue: `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
     },
     ci: {
-        id: "FormControlCI",
-        name: "CI",
+        id: "ci",
+        label: "CI",
         type: "text",
-        secondEntry: "Escriba el CI ",
-        text: "",
+        helperText: "Escriba el CI ",
+        required: true,
     },
     direccion: {
-        id: "FormControlDireccion",
-        name: "Dirección",
+        id: "direccion",
+        label: "Dirección",
         type: "text",
-        secondEntry: "Escriba la dirección",
-        text: "",
+        helperText: "Escriba la dirección",
+        required: true,
     },
     reparto: {
-        id: "FormControlReparto",
-        name: "Reparto",
+        id: "reparto",
+        label: "Reparto",
         type: "text",
-        secondEntry: "Escriba el reparto",
-        text: "",
+        helperText: "Escriba el reparto",
+        required: true,
     },
     municipio: {
-        id: "FormControlMunicipio",
-        name: "Municipio",
+        id: "municipio",
+        label: "Municipio",
         type: "text",
-        secondEntry: "Escriba el municipio",
-        text: "",
+        helperText: "Escriba el municipio",
+        required: true,
     },
     provincia: {
-        id: "FormControlProvincia",
-        name: "Provincia",
+        id: "provincia",
+        label: "Provincia",
         type: "text",
-        secondEntry: "Escriba la provincia",
-        text: "",
+        helperText: "Escriba la provincia",
+        required: true,
     },
     numero_telefono: {
-        id: "FormControlTelefono",
-        name: "Teléfono",
+        id: "numero_telefono",
+        label: "Teléfono",
         type: "text",
-        secondEntry: "Escriba el teléfono",
-        text: "",
+        helperText: "Escriba el teléfono",
+        required: true,
     },
     genero: {
-        id: "FormControlGenero",
-        name: "Género ",
-        type: "text",
-        secondEntry: "Escriba 'F' para femenino o 'M' para masculino",
-        values: "",
-        text: "",
+        id: "genero",
+        label: "Género ",
+        type: "select",
+        helperText: "Escriba 'F' para femenino o 'M' para masculino",
+        required: true,
+        values: [
+            {
+                id: "M",
+                name: "Masculino"
+            },
+            {
+                id: "F",
+                name: "Femenino"
+            }
+        ],
+
     },
     edad: {
-        id: "FormControlEdad",
-        name: "Edad",
+        id: "edad",
+        label: "Edad",
         type: "text",
-        secondEntry: "Escriba 'F' para femenino o 'M' para masculino",
-        text: "",
+        helperText: "Escriba 'F' para femenino o 'M' para masculino",
+        required: true,
     },
     nombre_apellido_padre: {
-        id: "FormControlPadre",
-        name: "Nombre y apellidos del padre",
+        id: "nombre_apellido_padre",
+        label: "Nombre y apellidos del padre",
         type: "text",
-        secondEntry: "Escriba el nombre y apellidos ",
-        text: "",
+        helperText: "Escriba el nombre y apellidos ",
+        required: true,
     },
     nombre_apellido_madre: {
-        id: "FormControlMadre",
-        name: "Nombre y apellidos de la madre",
+        id: "nombre_apellido_madre",
+        label: "Nombre y apellidos de la madre",
         type: "text",
-        secondEntry: "Escriba el nombre y apellidos ",
-        text: "",
+        helperText: "Escriba el nombre y apellidos ",
+        required: true,
     },
     grado: {
-        id: "FormControlGrado",
-        name: "Grado",
-        type: "text",
-        secondEntry: "Escriba el grado",
-        text: "",
+        id: "grado",
+        label: "Grado",
+        helperText: "Escriba el grado",
+        required: true,
+        type: "model",
     },
     grupo: {
-        id: "FormControlGrupo",
-        name: "Grupo",
+        id: "grupo",
+        label: "Grupo",
+        helperText: "Escriba el grupo",
+        required: true,
+        type: "model",
+    },
+    grado_academico: {
+        id: "grado_academico",
+        label: "Grado Académico",
+        helperText: "Seleccione el grado académico",
+        required: true,
+        type: "select",
+        values: [
+            {
+                id: "universitario",
+                name: "Universitario"
+            },
+            {
+                id: "master",
+                name: "Master en Ciencias"
+            },
+            {
+                id: "doctor",
+                name: "Doctor en Ciencias"
+            },
+        ]
+    },
+    categoria_docente: {
+        id: "categoria_docente",
+        label: "Categoría Docente",
+        helperText: "Seleccione la categoría docente",
+        required: true,
+        type: "select",
+        values: [
+            {
+                id: "profesor_titular",
+                name: "Profesor Titular"
+            },
+            {
+                id: "profesor_auxiliar",
+                name: "Profesor Auxiliar"
+            },
+            {
+                id: "asistente",
+                name: "Asistente"
+            },
+            {
+                id: "instructor",
+                name: "Instructor"
+            },
+            {
+                id: "instructor_auxiliar",
+                name: "Instructor Auxiliar"
+            },
+            {
+                id: "auxiliar_tecnico",
+                name: "Auxiliar Técnico de la Docencia"
+            },
+            {
+                id: "profesor_de_merito",
+                name: "Profesor de Mérito"
+            },
+            {
+                id: "profesor_invitado",
+                name: "Profesor Invitado"
+            },
+        ]
+    },
+    grupos: {
+        id: "grupos",
+        label: "Grupos",
+        required: false,
+        type: "modelList",
+        model: "grupo",
+        helperText: "Seleccione los grupos",
+    },
+    asignaturas: {
+        id: "asignaturas",
+        label:"Asignaturas",
+        required: false,
+        type: "modelList",
+        model: "asignatura",
+        helperText: "Seleccione las asignaturas",
+    },
+    numero_grupo: {
+        id: "numero_grupo",
+        label: "Número del grupo",
         type: "text",
-        secondEntry: "Escriba el grupo",
-        text: "",
-    }
+        helperText: "Escriba el número del Grupo",
+        required: true,
+    },
+    profesor: {
+        id: "profesor",
+        label: "Profesor",
+        helperText: "Seleccione el profesor",
+        required: true,
+        type: "model",
+    },
+    estudiante: {
+        id: "estudiante",
+        label: "Estudiante",
+        helperText: "Seleccione el estudiante",
+        required: true,
+        type: "model",
+    },
+    asignatura: {
+        id: "asignatura",
+        label: "Asignatura",
+        helperText: "Seleccione la asignatura",
+        required: true,
+        type: "model",
+    },
+    valor: {
+        id: "valor",
+        label: "Valor",
+        helperText: "Inserte el valor de la nota",
+        required: true,
+        type: "number",
+    },
+    tipo: {
+        id: "tipo",
+        label: "Tipo",
+        helperText: "Seleccione el tipo de nota",
+        required: true,
+        type: "select",
+        values: [
+            {
+                id: "es",
+                name: "Evaluación Sistemática"
+            },
+            {
+                id: "ep",
+                name: "Evaluación Parcial"
+            },
+            {
+                id: "ef",
+                name: "Evaluación Final"
+            }
+        ],
+    },
+    curso: {
+        id: "curso",
+        label: "Curso",
+        helperText: "Seleccione el curso",
+        required: true,
+        type: "model",
+    },
+    archivo: {
+        id: "archivo",
+        label: "Archivo",
+        helperText: "Inserte el archivo",
+        required: true,
+        type: "file",
+    },
+
 
 }
 
--- src/data/tableFieldDescription.js
+++ src/data/tableFieldDescription.js
@@ -1,32 +1,159 @@
 const tableFieldDescription = {
-    id:"ID",
-    nombre: "Nombre",
-    primer_apellido: "Primer Apellido",
-    segundo_apellido: "Segundo Apellido",
-    fecha_nacimiento: "Fecha de nacimiento",
-    ci: "CI",
-    direccion: "Dirección",
-    reparto: "Reparto",
-    municipio: "Municipio",
-    provincia: "Provincia",
-    numero_telefono: "Teléfono",
-    genero: "Género ",
-    edad:"Edad",
-    nombre_apellido_padre: "Padre",
-    nombre_apellido_madre: "Madre",
-    grado:"Grado",
-    grupo:"Grupo",
-    categoria_docente:"Categoria Docente",
-    asignatura: "Asignatura",
-    grupos: "Grupos",
-    estudiante: "Estudiante",
-    valor: "Valor",
-    tipo: "Tipo",
-    curso: "Curso",
-    archivo: "Archivo",
-    nivel_de_estudio: "Nivel de Estudio",
-    categoria_cientifica: "Categoria Cientifica",
-    profesor: "Profesor",
-    numero_grupo: "Numero"
+    id: {
+        id: "id",
+        label: "ID",
+        type: "text"
+    },
+    nombre: {
+        id: "nombre",
+        label: "Nombre",
+        type: "text"
+    },
+
+    primer_apellido: {
+        id: "primer_apellido",
+        label: "Primer Apellido",
+        type: "text"
+    },
+    segundo_apellido: {
+        id: "segundo_apellido",
+        label: "Segundo Apellido",
+        type: "text"
+    },
+    fecha_nacimiento: {
+        id: "fecha_nacimiento",
+        label: "Fecha de nacimiento",
+        type: "text"
+    },
+    ci: {
+        id: "ci",
+        label: "CI",
+        type: "text"
+    },
+    direccion: {
+        id: "direccion",
+        label: "Dirección",
+        type: "text"
+    },
+    reparto: {
+        id: "reparto",
+        label: "Reparto",
+        type: "text"
+    },
+    municipio: {
+        id: "municipio",
+        label: "Municipio",
+        type: "text"
+    },
+    provincia: {
+        id: "provincia",
+        label: "Provincia",
+        type: "text"
+    },
+    numero_telefono: {
+        id: "numero_telefono",
+        label: "Teléfono",
+        type: "text"
+    },
+    genero: {
+        id: "genero",
+        label: "Género ",
+        type: "text"
+    },
+    edad: {
+        id: "edad",
+        label: "Edad",
+        type: "text"
+    },
+    nombre_apellido_padre: {
+        id: "nombre_apellido_padre",
+        label: "Padre",
+        type: "text"
+    },
+    nombre_apellido_madre: {
+        id: "nombre_apellido_madre",
+        label: "Madre",
+        type: "text"
+    },
+    grado: {
+        id: "grado",
+        label: "Grado",
+        type: "model"
+    },
+    grupo: {
+        id: "grupo",
+        label: "Grupo",
+        type: "model"
+    },
+    categoria_docente: {
+        id: "categoria_docente",
+        label: "Categoria Docente",
+        type: "text"
+    },
+    asignatura: {
+        id: "asignatura",
+        label: "Asignatura",
+        type: "model"
+    },
+    grupos: {
+        id: "grupos",
+        label: "Grupos",
+        type: "modelList"
+    },
+    estudiante: {
+        id: "estudiante",
+        label: "Estudiante",
+        type: "model"
+    },
+    valor: {
+        id: "valor",
+        label: "Valor",
+        type: "text"
+    },
+    tipo: {
+        id: "tipo",
+        label: "Tipo",
+        type: "select",
+        values: {
+            es: "Evaluación Sistemática",
+            ep: "Evaluación Parcial",
+            ef: "Evaluación Final"
+        }
+    },
+    curso: {
+        id: "curso",
+        label: "Curso",
+        type: "model"
+    },
+    archivo: {
+        id: "archivo",
+        label: "Archivo",
+        type: "text"
+    },
+    nivel_de_estudio: {
+        id: "nivel_de_estudio",
+        label: "Nivel de Estudio",
+        type: "text"
+    },
+    grado_academico: {
+        id: "grado_academico",
+        label: "Grado Académico",
+        type: "text"
+    },
+    profesor: {
+        id: "profesor",
+        label: "Profesor",
+        type: "model"
+    },
+    numero_grupo: {
+        id: "numero_grupo",
+        label: "Numero",
+        type: "text"
+    },
+    asignaturas: {
+        id: "asignaturas",
+        label: "Asignaturas",
+        type: "modelList"
+    },
 }
 export default tableFieldDescription;
