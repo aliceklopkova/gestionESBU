bd2160283cc58f610ac0e6ac9b875156002ee581
Abel Cuza <abel.cuza07@gmail.com>
2022-11-21 22:40:43 -0500

    Update table edit and delete links and add custom cell

--- src/components/Contenidotabla.jsx
+++ src/components/Contenidotabla.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import {Link, useLocation} from "react-router-dom";
 import './Contenidotabla.css';
+import tableFieldDescription from "../data/tableFieldDescription";
+import CustomCell from "./table-cells/CustomCell";
+
 
 function ContenidoTabla({count, item, fields}) {
     const location = useLocation()
     return (
         <tr>
-            {fields.map((field) => (
-                <td key={field}>{item[field]}</td>
-            ))}
+            {fields.map((field) => <CustomCell key={field} data={item[field]} description={tableFieldDescription[field]}/>)}
+
             <td className={'material-symbols-outlined '}>
                 <Link className={'icono'} style={{textDecoration: 'none'}}
-                    to={`${location.pathname}/${item['ci']}`}>edit_square</Link></td>
+                      to={`${location.pathname}/${item['id']}`}>edit_square</Link></td>
             <td className={'material-symbols-outlined '}>
-                <Link className={'borrarcolor'} style={{textDecoration: 'none'}} to={`${location.pathname}/${item['ci']}/delete`}>delete</Link>
+                <Link className={'borrarcolor'} style={{textDecoration: 'none'}}
+                      to={`${location.pathname}/${item['id']}/delete`}>delete</Link>
             </td>
         </tr>
 
