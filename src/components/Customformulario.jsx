6eec0bc792611af9da7c283cae24ff8219333b7a
Abel Cuza <abel.cuza07@gmail.com>
2022-11-21 22:37:09 -0500

    Add navigation for form buttons and update layout padding

--- src/components/Customformulario.jsx
+++ src/components/Customformulario.jsx
@@ -1,22 +1,25 @@
 import React, {useEffect, useState} from 'react';
-import {Link, useLocation, useParams} from 'react-router-dom'
 import './Customformulario.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import CampoFormulario from "./Campoformulario";
+import CustomField from "./form/custom-field";
+import {Box, Button, Grid, Stack} from "@mui/material";
 import formFieldDescription from "../data/formFieldDescription";
+import {useLocation, Link} from "react-router-dom";
+import {useParams, useNavigate} from "react-router";
+
 
 function CustomForm({fields, model, api, modificar}) {
     const location = useLocation()
     const [data, setData] = useState([])
     const [modData, setModData] = useState([])
     const {id} = useParams()
+    const navigate = useNavigate()
     useEffect(() => {
         if (modificar) {
             api(id).get().then((items) => {
                 if (items.status === 200) {
                     setModData(items.data)
                     setData(items.data)
+
                 } else {
                     setModData("error")
                 }
@@ -27,42 +30,43 @@ function CustomForm({fields, model, api, modificar}) {
     const handleSubmit = (event) => {
         event.preventDefault()
         modificar ?
-            api(id).update(data).then((res) => (
+            api(id).update(data).then((res) => {
                 console.log(res)
-            ))
+                navigate(`/${location.pathname.split("/")[1]}`)
+                // return <Navigate replace to={`/${location.pathname.split("/")[1]}`}/>
+            })
             :
-            api().create(data).then((res) => (
+            api().create(data).then((res) => {
                 console.log(res)
-            ))
+                navigate(`/${location.pathname.split("/")[1]}`)
+                // return <Navigate replace to={`/${location.pathname.split("/")[1]}`}/>
+            })
     }
     return (
-        <div className={'Formsize'}>
-            <Form onSubmit={handleSubmit}>
-                {!modificar ? <div>
-                    {fields.filter(field=>field !== 'id').map((item) => (
-                            <CampoFormulario key={item} campo={item} data={formFieldDescription[item]} setData={setData}/>
-                        )
-                    )}
-                </div> : <div>
-                    {fields.filter(field=>field !== 'id').map((key) => (
-                            <CampoFormulario key={key} campo={key} setData={setData}
-                                             data={{...formFieldDescription[key], values: modData[key]}}/>
-                        )
-                    )}
-                </div>}
-                <div className={'Botonsubmit'}>
-                    <Button style={{width: '100px'}} variant="success" type="submit">Guardar</Button>
+        <Box component="form" sx={{flexGrow: 1, padding: "50px", width:"100%"}} onSubmit={handleSubmit}>
+            <Grid container spacing={2}>
+                {!modificar ?
+                    fields.filter(field => field !== 'id').map((item) => (
+                        <Grid xs={12} md={3} sm={6}>
+                            <CustomField  {...formFieldDescription[item]} setParams={setData} sx={{width: "90%", margin:"15px 0"}}/>
+                        </Grid>
+                    ))
+                    :
+                    fields.filter(field => field !== 'id').map((key) => (
+                        <Grid xs={12} md={3} sm={6}>
+                            <CustomField  {...formFieldDescription[key]} setParams={setData} defaultValue={modData[key]} sx={{width: "90%", margin:"15px 0"}}/>
+                        </Grid>
+                    ))
+                }
+            </Grid>
+            <Stack sx={{marginTop:"15px"}} direction="row" spacing={2}>
+                    <Button sx={{width: '100px'}} variant="contained" color="success" type="submit">Guardar</Button>
+                    <Link style={{textDecoration: "none"}} to={`/${location.pathname.split("/")[1]}`}>
+                        <Button sx={{width: '100px'}} variant="contained" color="error" type="">Cancelar</Button>
+                    </Link>
+            </Stack>
+        </Box>
 
-                </div>
-                <div className={'botondanger'}>
-                    <Link to={`/${location.pathname.split("/")[1]}`}>
-                     <Button style={{width: '100px'}} variant="danger" type="">Cancelar</Button>
-                </Link>
-
-                </div>
-
-            </Form>
-        </div>
     );
 }
 
