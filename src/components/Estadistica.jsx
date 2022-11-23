324538045e358b1c0527364ef1e9c54dc163a805
Alicia Klopkova <aaportela73@gmail.com>
2022-11-23 15:15:50 -0500

    se creo el hooks para trabajar con los datos de la api y se crearon las funcionalidades cantidad de estudiantes, profesores, grupos y asignaturas.

--- src/components/Estadistica.jsx
+++ src/components/Estadistica.jsx
@@ -1,32 +1,57 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './Estadistica.css';
 import Cardr from "./Card";
 import Graficabarra from "./Graficabarra";
+import api from "../api/index"
 
 function Estadistica() {
+    const [data, setData] = useState({})
+    useEffect(() => {
+        api.estudiante().list().then(resp => {
+            setData((oldData) => (
+                {...oldData, estudiante: resp.data.length}
+            ))
+        })
+        api.profesor().list().then(resp => {
+            setData((oldData) => (
+                {...oldData, profesor: resp.data.length}
+            ))
+        })
+        api.grupo().list().then(resp => {
+            setData((oldData    ) => (
+                {...oldData, grupo: resp.data.length}
+            ))
+        })
+        api.asignatura().list().then(resp => {
+            setData((oldData) => (
+                {...oldData, asignatura: resp.data.length}
+            ))
+        })
+
+    }, [])
+    console.log(data);
     return (
         <div className={'estadistic'}>
             <div className={'posicion_card'}>
-                <Cardr style={"colore"} symbol="face" name="Cantidad de Estudiantes"
-                       number="500"/>
+                <Cardr url="estudiante" style={"colore"} symbol="face" name="Estudiantes"
+                       number={data.estudiante}/>
             </div>
-                <div className={'posicion_card'}>
-                     <Cardr style={"colore-b"} symbol="face_4" name="Cantidad de Profesores"
-                       number="40"/>
-                </div>
-             <div className={'posicion_card'}>
-                     <Cardr style={"colore-c"} symbol="group" name="Cantidad de Grupos"
-                       number="40"/>
-                </div>
             <div className={'posicion_card'}>
-                     <Cardr style={"colore-d"} symbol="workspaces" name="Cantidad de Aulas"
-                       number="40"/>
-                </div>
+                <Cardr url="profesor" style={"colore-b"} symbol="face_4" name="Profesores"
+                       number={data.profesor}/>
+            </div>
+            <div className={'posicion_card'}>
+                <Cardr url="grupo" style={"colore-c"} symbol="group" name="Grupos"
+                       number={data.grupo}/>
+            </div>
+            <div className={'posicion_card'}>
+                <Cardr url="asignatura" style={"colore-d"} symbol="workspaces" name="Asignaturas"
+                       number={data.asignatura}/>
+            </div>
             <div className={'posicion_graf'}>
                 <p className={'text_a'}>Promedio por grado y del curso escolar</p>
                 <Graficabarra/>
             </div>
-
         </div>
 
     );
