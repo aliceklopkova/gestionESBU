a466b1da07eaf0881a2879ea5c2c6faa1575657a
Abel Cuza <abel.cuza07@gmail.com>
2022-11-23 23:58:10 -0500

    Add graficas styles and endpoint calls

--- src/components/Graficabarra.jsx
+++ src/components/Graficabarra.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Graficabarra.css';
+import estudianteApi from "../api/estudiante";
+import graficas from "../data/graficas";
 
-function Graficabarra(){
-    return(
-        <div >
-            <p className={'texto'}>7mo</p>
-            <div className="containerd">
-                <div className="skills html">90%</div>
-            </div>
+function Graficabarra() {
+    const [promedio, setPromedio] = useState({})
 
-            <p className={'texto'}>8vo</p>
-            <div className="containerd">
-                <div className="skills css">80%</div>
-            </div>
+    const promedioGeneral = () => (
+        (promedio.septimo + promedio.octavo + promedio.noveno) / 3
+    )
+    const skillsStyle = {
+        textAlign: "right",
+        paddingTop: "10px",
+        paddingBottom: "10px",
+        color: "midnightblue",
+    }
+    useEffect(() => {
+        estudianteApi().get_promedio("1").then(resp => {
+            setPromedio(oldData => (
+                {...oldData, septimo: resp.data['promedio']}
+            ))
+        })
+        estudianteApi().get_promedio("2").then(resp => {
+            setPromedio(oldData => (
+                {...oldData, octavo: resp.data['promedio']}
+            ))
+        })
+        estudianteApi().get_promedio("3").then(resp => {
+            setPromedio(oldData => (
+                {...oldData, noveno: resp.data['promedio']}
+            ))
+        })
 
-            <p className={'texto'}>9no</p>
-            <div className="containerd">
-                <div className="skills js">65%</div>
-            </div>
+    }, [])
 
-            <p className={'texto'}>Curso</p>
-            <div className="containerd">
-                <div className="skills php">60%</div>
+    useEffect(()=>{
+        setPromedio(oldData => (
+            {...oldData, general: promedioGeneral()}
+        ))
+    }, [promedio.septimo, promedio.octavo, promedio.noveno])
+
+    return (
+        graficas.map(({id, label, style}) => (
+            <div key={id}>
+                <p className={'texto'}>{label}</p>
+                <div className="containerd">
+                    <div style={{...skillsStyle, ...style, width: `${promedio[id]}%`}}>{promedio[id]}</div>
+                </div>
             </div>
-        </div>
+        ))
 
     );
 }
+
 export default Graficabarra;
---
+++ src/data/graficas.js
@@ -0,0 +1,24 @@
+const graficas = [
+    {
+        id: "septimo",
+        label: "7mo",
+        style: {backgroundColor: "rgb(137, 138, 166)"},
+    },
+    {
+        id: "octavo",
+        label: "8vo",
+        style: {backgroundColor: "rgb(201, 187, 207)"},
+    },
+    {
+        id: "noveno",
+        label: "9no",
+        style: {backgroundColor: "rgb(183, 211, 223)"},
+    },
+    {
+        id: "general",
+        label: "General",
+        style: {backgroundColor: "rgb(214, 239, 237)"},
+    },
+]
+
+export default graficas
