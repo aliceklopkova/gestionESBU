5d401a3e565911f42b426caa41b33193bdcf67f2
Abel Cuza <abel.cuza07@gmail.com>
2022-11-23 23:55:16 -0500

    Add get promedio endpoint

--- src/api/estudiante.js
+++ src/api/estudiante.js
@@ -1,10 +1,15 @@
 import getApi from "./base/get-api";
-
+import {post} from "./base";
 const {REACT_APP_BACKEND_API} = process.env;
 const endpoint = `${REACT_APP_BACKEND_API}/estudiante/`;
+const promedio_url = "promedio_grado"
 
 const estudianteApi = (id) => (
-    {...getApi(endpoint, id)}
+    {...getApi(endpoint, id),
+        get_promedio: (grado) => {
+        return post(`${endpoint}${promedio_url}/`, {grado:grado})
+        }
+    }
 )
 
 
